720e740d5cdf6c52438f22c56afc7779fd757142
Viktor Hirenko <viktor.hirenko@gmail.com>
2020-01-20 16:03:06 +0200

    edit js file

--- src/js/script.js
+++ src/js/script.js
@@ -5,18 +5,62 @@
 })(jQuery);
 
 const page = document.querySelector('.page');
+const modalBackdrop = document.querySelector('.modal-backdrop');
 const orderForm = document.querySelector('.order-form');
 const openModalBtns = document.querySelectorAll(
   '.button[data-action="open-modal"]',
+);
+const closeModalBtn = orderForm.querySelector(
+  '.button[data-action="close-modal"]',
 );
 
 openModalBtns.forEach(function(item) {
   item.addEventListener('click', handleOpenModal);
 });
+closeModalBtn.addEventListener('click', handleCloseModalBtnClick);
+modalBackdrop.addEventListener('click', handleModalBackdropClick);
 
 function handleOpenModal(evt) {
   evt.preventDefault();
 
   page.classList.add('show-modal');
   window.addEventListener('keydown', handleModalEscPress);
+}
+
+function handleModalBackdropClick(evt) {
+  const target = evt.target;
+  const nodeName = target.nodeName;
+
+  const parent = this.closest('.modal-backdrop');
+
+  if (nodeName !== 'DIV') return;
+
+  handleCloseModal(nodeName);
+}
+
+function handleCloseModalBtnClick(evt) {
+  const target = evt.target;
+  const nodeName = target.nodeName;
+
+  if (nodeName !== 'BUTTON') return;
+
+  handleCloseModal(nodeName);
+
+  evt.preventDefault();
+}
+
+function handleCloseModal() {
+  // remove show-madal class from page
+  page.classList.remove('show-modal');
+  // remove event listener handleModalEscPress from window
+  window.removeEventListener('keydown', handleModalEscPress);
+}
+
+function handleModalEscPress(evt) {
+  // determine the code of the pressed button and
+  const key = evt.code;
+  // close madal if the escape key was pressed
+  if (key !== 'Escape') return;
+
+  handleCloseModal();
 }
