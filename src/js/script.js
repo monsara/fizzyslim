6a4975b901ca62160719cfcd90b07f2e03730ad6
Viktor Hirenko <viktor.hirenko@gmail.com>
2020-01-20 02:13:51 +0200

    separate markup and styles into chunks

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,20 @@
     $('.styler').styler();
   });
 })(jQuery);
+
+const page = document.querySelector('.page');
+const orderForm = document.querySelector('.order-form');
+const openModalBtns = document.querySelectorAll(
+  '.button[data-action="open-modal"]',
+);
+
+openModalBtns.forEach(function(item) {
+  item.addEventListener('click', handleOpenModal);
+});
+
+function handleOpenModal(evt) {
+  evt.preventDefault();
+
+  page.classList.add('show-modal');
+  window.addEventListener('keydown', handleModalEscPress);
+}
